e87939d5fe5a899d13a4017752dc1b66ed84b349
Vibuda Siromin <sirominkv.20@uom.lk>
2025-03-24 20:12:43 +0530

    Initial commit

---
+++ BlazorApp1/Services/INumberService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.Services
+{
+    public interface INumberService
+    {
+         int Number1 { get; }
+         int Number2 { get; }
+    }
+}
---
+++ BlazorApp1/Services/NumberService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Services
+{
+    public class NumberService : INumberService
+    {
+        public int Number1 { get; private set; }
+
+        public int Number2 { get; private set; }
+
+        public NumberService()
+        {
+            Number1 = Random.Shared.Next(1, 1000);
+            Number2 = Random.Shared.Next(1, 1000);
+        }
+    }
+}
